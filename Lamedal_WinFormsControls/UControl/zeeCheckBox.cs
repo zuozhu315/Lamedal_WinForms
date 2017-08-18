88fe32e9b4c3af333946b3525366281d200127f8
PerezLamed <perez.lamed.vanniekerk@gmail.com>
2017-08-18 22:00:15 +0200

    Updated vsix with combobox and test

---
+++ Lamedal_WinFormsControls/UControl/zeeCheckBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using Lamedal_UIWinForms.UControl._Designer;
+
+namespace Lamedal_WinFormsControls.UControl
+{
+    [ProvideToolboxControl("Lamedal_WinFormsControls.UControl.zeeCheckBox", false)]
+    [Description("CheckBox control")]
+    [Serializable]
+    //[DefaultEvent("Event_OnValueChange")]
+    [DefaultProperty("Name")]
+    //[Designer(typeof(UControl_Interface_Designer))] // Link the designer   
+    //[Docking(DockingBehavior.Ask)] // Dock in parent control
+    //[ToolboxItem(true)]
+    [ToolboxBitmap("Resources.checkbox_16xLG.bmp")]
+    public class zeeComboBox : ComboBox_
+    {
+    }
+}
---
+++ Lamedal_WinFormsControls/UControl/zeeComboBox.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using Lamedal_UIWinForms.UControl._Designer;
+
+namespace Lamedal_WinFormsControls.UControl
+{
+    [ProvideToolboxControl("Lamedal_WinFormsControls.UControl.zeeCheckBox", false)]
+    public class zeeCheckBox : CheckBox_
+    {
+    }
+}
